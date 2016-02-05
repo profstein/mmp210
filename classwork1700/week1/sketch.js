ff177a2b1311e1a55ebff5ce7aa082d8b5c3fcfd
Owen Roberts <oroberts@f1006-i-34171.bmcc.cuny.edu>
2016-02-04 20:30:10 -0500

    updates 1700 classwork week 1

---
+++ classwork1700/week1/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+    createCanvas(640, 480);
+}
+
+function draw() {
+    
+    //rect(50, 50, 300, 300);
+    
+    fill(0);
+    stroke(0, 0, 255);
+    
+    ellipse(100, 100, 25, 25);
+
+    fill(255);
+    stroke(0, 255, 255);
+    
+    ellipse(200, 300, 25, 25);
+    
+    triangle(30, 75, 58, 20, 86, 75);
+    
+}

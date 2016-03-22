fafda8c33a7bf87867ec7d51b0cc18101c60bc63
Owen Roberts <oroberts@f706-i-34138.bmcc.cuny.edu>
2016-03-22 14:39:33 -0400

    week 6 1400 examples

---
+++ classwork1400/week7/text.js
@@ -0,0 +1,39 @@
+function setup() {
+    createCanvas(640, 480);
+    background("white");
+
+    var lineSize = 70;
+
+    textSize(lineSize);
+    
+    var y = lineSize;
+    
+    var str1 = "Hello world.";
+    var str2 = "你好，世界";
+    var str3 = "Strings can also include numbers like 100, punctuation like !~+:) and special characters like π or © or ∆";
+    
+    fill("blue");
+    strokeWeight(5);
+    stroke("green");
+    
+    str1 = str1.replace("Hello", "");
+    str1 = "Goodbye" + str1;
+    text(str1, 20, y);
+    
+    y += lineSize;
+    text(str2[str2.length - 1], 20, y);
+    
+    textFont("monaco");
+    
+    textStyle(ITALIC);
+    textAlign(RIGHT);
+   
+    y += lineSize;
+    textSize(30);
+    textLeading(50);
+    text(str3, 20, y, width - 50, 200);
+}
+
+
+
+

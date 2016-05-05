99d45e586069820b7f4741bf6d6f70f703beb871
Owen Roberts <obr208@nyu.edu>
2016-05-05 19:58:37 -0400

    platform game with graphics

--- classwork1700/week13/game.js
+++ classwork1700/week13/game.js
@@ -1,5 +1,5 @@
 var player;
-var speed = 0.2;
+var speed = 10;
 var jump = 20;
 var jumping = false;
 
@@ -9,8 +9,8 @@ var player_walk_sprites,
 	player_stand;
 
 var gravity = 1;
-var platform;
-var obstacle;
+var platform, ground;
+var obstacle, fire;
 
 function preload() {
 	player_walk_sprites = loadSpriteSheet("player.png", 64, 64, 8);
@@ -18,6 +18,9 @@ function preload() {
 	
 	player_stand_sprites = loadSpriteSheet("player.png", 64, 64, 1);
 	player_stand = loadAnimation(player_stand_sprites);
+	
+	fire = loadImage("fire.png");
+	ground = loadImage("ground.png");
 }
 
 function setup() {
@@ -30,7 +33,14 @@ function setup() {
 	//player.debug = true;
 	
 	platform = createSprite(width/2, height, width, 20);
+	platform.addImage(ground);
+	platform.setCollider("rectangle", 0, 0, width, 10);
+	//platform.debug = true;
+	
 	obstacle = createSprite(300, height, 60, 40);
+	obstacle.addImage(fire);
+	obstacle.setCollider("circle", 0, 0, 32, 32);
+	//obstacle.debug = true;
 }
 
 function draw() {
@@ -41,15 +51,16 @@ function draw() {
 		jumping = true;
 	}
 	
-	if ( keyDown("a") ) {
-		player.velocity.x -= speed;	
-		player.changeAnimation("walk");
-	} else if ( keyDown("d") ) {
-		player.velocity.x += speed;	
+	if ( keyDown("d") ) {
+		obstacle.position.x -= speed;
+		platform.position.x -= speed;
 		player.changeAnimation("walk");
 	} else {
 		player.changeAnimation("stand");	
 	}
+	
+	if (platform.position.x < 0) platform.position.x = width/2;
+	if (obstacle.position.x < -obstacle.width) obstacle.position.x = random(width, width*2);
 	
 	player.velocity.y += gravity;
 	if ( player.collide(platform) ) {
